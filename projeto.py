8be197cc91bdf6987365e391abb33bce2f8e2541
Daniel <danieloliveiradesouza09@outlook.com>
2025-05-29 20:40:06 -0300

    prototipo inicial menu

--- projeto.py
+++ projeto.py
@@ -1,23 +1,45 @@
 print("\n\nBem-vindo a simulação de enchentes da Fluxo Zero™")
-print("================================================\n\n")
+print("=================================================\n\n")
 
-def menu():
-    print("\n")
+def menu_inicial():
     print("1 - Iniciar simulação.") # Acessa a simulação
     print("2 - Histórico de simulações anteriores.") # Acessa o json
     print("3 - Sair do programa.")
-    escolha = input("\nEscolha uma opção:")
+    escolha = input("\nEscolha uma opção: ")
     return escolha
 
+def menu_simulacao():
+    while True:
+        print(":: Simulação de enchentes ::")
+        print("\n")
+        print("a. Configurar simulação.")
+        print("b. Iniciar simulação.")
+        print("c. Fechar simulação.")
+        opcao = input("\nEscolha uma opção: ")
+
+        match opcao:
+            case "a":
+                pass
+            case "b":
+                pass
+            case "c":
+                print("Fechando simulação...\n")
+                break
+            case _:
+                print("Insira um valor válido!\n")
+
+
+
 while True:
-    opcao = menu()
+    opcao = menu_inicial()
     match opcao:
         case "1":
-            print("Iniciando simulação...")
+            print("Iniciando simulação...\n")
+            menu_simulacao()
         case "2":
-            print("Abrindo histórico da simulação...")
+            print("Abrindo histórico da simulação...\n")
         case "3":
-            print("Saindo do programa...")
+            print("Saindo do programa...\n")
             break
         case _:
-            print("Insira um valor válido!")
+            print("Insira um valor válido!\n")
